f080e8a74dcb1948780389f45cd2862091f8a0b6
Michael Ivey <michael.ivey@riotgames.com>
2013-05-14 19:32:55 -0700

    Deps

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,4 @@ description      "Installs/Configures berkshelf-cookbook-fixture"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
 
+depends 'hostsfile', '1.0.1'
